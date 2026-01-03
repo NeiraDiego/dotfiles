26f789c7d75985f7ab9156fcfb47e470df6bd445
Diego Neira <diegoneira@gmail.com>
2026-01-03 13:08:25 -0300

    actualizacion neovim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,13 +1,13 @@
 return {
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function ()
+    config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "pyright" }
       })
@@ -18,13 +18,28 @@ return {
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities
-      })
+
+      -- Configuración para lua_ls usando la nueva API de Neovim 0.11+
+      vim.lsp.config.lua_ls = {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
+        capabilities = capabilities,
+      }
+
+      -- Configuración para pyright usando la nueva API de Neovim 0.11+
+      vim.lsp.config.pyright = {
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" },
+        capabilities = capabilities,
+      }
+
+      -- Habilitar los servidores LSP
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('pyright')
+
+      -- Keymaps para LSP
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
